ba26f65ad66dbc319a7a51567a003518c25e5e88
hartley9 <k.hartley1@lancaster.ac.uk>
2022-11-30 00:11:09 +0000

    autogenerate service map, automatically add event listeners for each service register

---
+++ node_content/dist/maxServiceObjectGenerator/constants.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NEWLINE = void 0;
+exports.NEWLINE = '\n';
---
+++ node_content/src/maxServiceObjectGenerator/clearOutputDir.ts
@@ -0,0 +1,19 @@
+/* const rimraf = require('rimraf');
+//remove files in output dir
+(async () => rimraf('./output', () => console.log('cleared output dir...'))); */
+
+
+const fsi = require("fs");
+const path = require("path");
+
+const directory = "./src/maxServiceObjectGenerator/autogen_src/";
+
+fsi.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fsi.unlink(path.join(directory, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
